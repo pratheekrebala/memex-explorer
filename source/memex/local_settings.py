77b02a51f1a19d7ab7c88df4b003f8d2cd0c368a
Pratheek Rebala <pratheekrebala@gmail.com>
2016-02-09 12:56:42 -0500

    Deploy config
    Preparing for deploy
    
    Signed-off-by: Pratheek Rebala <pratheekrebala@gmail.com>

--- source/memex/local_settings.py
+++ source/memex/local_settings.py
@@ -1,4 +1,4 @@
 #should store variables specific to a server. This is * imported into settings.py
-HOSTNAME='example.com'
+HOSTNAME='memex.rwradvisory.com'
 IP_ADDR='0.0.0.0'
 ROOT_MEMEX_PORT='8000'
